c3d58bef56ef8fc63ddf5f9609b451dfcb149daf
Sohan Maali <sohanmaali4.com>
2023-12-04 17:18:25 +0530

    mail

--- src/java/SendMail.java
+++ src/java/SendMail.java
@@ -11,18 +11,21 @@ import java.util.Scanner;
 
 public class SendMail {
 
-    private String msgSubject = new String();
-    private String msgText = new String();
-    private final String USER_NAME = "";   // Reasever Mail (User Mail)
-    final String PASSSWORD = "";  //Password of the Goole(gmail) account
-    final static String SENDER = "";  // Sender gmail addresss
+    private String msgSubject;  //Mail Subject
+    private String msgText;     //Mail Massage
+    private String userName;   // Reasever Mail (User Mail)
+    private final static String PASSSWORD = "myax kkxr apwj wpgh";  //Password of the Google Account (gmail) account Generate
+    private final static String SENDER = "sohaninfobeans4@gmail.com";  // Sender gmail addresss
 
     public static void main(String[] args) {
         SendMail email = new SendMail();
-        //Sending test email
         int otp = otpGenerat();
+        email.setMsgSubject("Account varification in Swastik..."); // set Subject
+        email.setMsgText(" Dear swastik user. ,\n the one time OTP to reset your password at (swastik Account) is " + otp); // set Message
+        email.sendEmailMessage(email); // Method Call for Send Mail
+
+//        ----------------------------------------------------
         Scanner sc = new Scanner(System.in);
-        email.createAndSendEmail(SENDER, "Account varification in Swastik...", " Dear swastik user. ,\n the one time OTP to reset your password at (swastik Account) is " + otp + ".\n \n This OTP will expire in 5 minutes. ");
         int fillOtp = sc.nextInt();
         if (otp == fillOtp) {
             System.out.println("Registration Seccsess");
@@ -31,13 +34,7 @@ public class SendMail {
         }
     }
 
-    public void createAndSendEmail(String emailAddressTo, String msgSubject, String msgText) {
-        this.msgSubject = msgSubject;
-        this.msgText = msgText;
-        sendEmailMessage();
-    }
-
-    private void sendEmailMessage() {
+    private void sendEmailMessage(SendMail email) {
 
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
@@ -48,18 +45,19 @@ public class SendMail {
         Session session = Session.getInstance(props, new jakarta.mail.Authenticator() {
             @Override
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(SENDER, PASSSWORD);
+                return new PasswordAuthentication(SendMail.SENDER, SendMail.PASSSWORD);
             }
         });
 
         try {
             Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(SENDER)); //Set from address of the email
-            message.setContent(msgText, "text/html"); //set content type of the email
 
-            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(USER_NAME));
+            message.setFrom(new InternetAddress(SendMail.SENDER)); //Set from address of the email
+            message.setContent(email.getMsgText(), "text/html"); //set content type of the email
 
-            message.setSubject(msgSubject); //Set email message subject
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email.getUserName()));   //Set User Mail
+
+            message.setSubject(email.getMsgSubject()); //Set email message subject
             Transport.send(message); //Send email message
 
             System.out.println("sent email successfully!");
@@ -67,6 +65,8 @@ public class SendMail {
         } catch (MessagingException e) {
 
             System.out.println(e);
+            System.out.println("Mail Not Send"); //if  Get Any Exception
+
         }
     }
 
@@ -74,5 +74,29 @@ public class SendMail {
         int random = (int) (Math.random() * 999999);
         System.out.println("rendom num is: " + random);
         return random;
+    }
+
+    public String getMsgSubject() {
+        return msgSubject;
+    }
+
+    public void setMsgSubject(String msgSubject) {
+        this.msgSubject = msgSubject;
+    }
+
+    public String getMsgText() {
+        return msgText;
+    }
+
+    public void setMsgText(String msgText) {
+        this.msgText = msgText;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 }
